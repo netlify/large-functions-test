11b1dbaaccac3949235347f0e7c94a30c56cf019
Ivan <ivan.zarea@netlify.com>
2022-07-05 07:06:11 +0200

    deleted a comment to change the checksum

--- netlify/functions/size-9mb/size-9mb.js
+++ netlify/functions/size-9mb/size-9mb.js
@@ -5,9 +5,6 @@ const handler = async (event) => {
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
     }
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
